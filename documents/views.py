79fb0901ae86d2b9a7a0a32497bc50f37975ce73
zalasek <zalesnyjakub@gmail.com>
2021-10-22 12:20:07 +0200

    a

--- documents/views.py
+++ documents/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from .models import Document
 # Create your views here.
 
+
+
 def DocumentListView(request, *args, **kwargs):
     documents = Document.objects.all()
     context = {'documents' : documents}
     return render(request, 'documents/document_list.html', context)
 
+
+
 def DocumentDetailView(request, pk):
-    document = Document.objects.filter(id=pk)
+    document = Document.objects.get(id=pk)
     context = {'document':document}
-    return render(request, 'documents/document_view.html', context)
+    return render(request, 'documents/document_detail.html', context)
     
